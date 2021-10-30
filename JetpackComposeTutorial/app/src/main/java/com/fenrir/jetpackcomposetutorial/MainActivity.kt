002486061d3b9fc753dcc8d06aa99a09699117f2
yugo-miyabe <yuni42yuyuyu@gmail.com>
2021-10-31 00:21:11 +0900

    Text要素を追加

--- JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
+++ JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
@@ -14,25 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Text(text = "Hello world!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
     }
 }
