9e0fa859ae5ac376c61fbd045d1d003240628c80
yugo-miyabe <yuni42yuyuyu@gmail.com>
2021-10-31 21:50:05 +0900

    画像リソースを表示

--- JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
+++ JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
@@ -3,9 +3,12 @@ package com.fenrir.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +23,16 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MessageCard(msg: Message) {
-        Column {
-            Text(text = msg.author)
-            Text(text = msg.body)
+        Row {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "Contact profile picture",
+            )
+            Column {
+                Text(text = msg.author)
+                Text(text = msg.body)
+            }
+
         }
     }
 
