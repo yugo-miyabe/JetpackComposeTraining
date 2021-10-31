060cbf03da15aaadd7d6da34b5f6bb1f98c905cb
yugo-miyabe <yuni42yuyuyu@gmail.com>
2021-10-31 21:03:22 +0900

    複数のテキストを追加

--- JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
+++ JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
@@ -5,17 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 
+    data class Message(val author: String, val body: String)
+
     @Composable
-    fun MessageCard(name: String) {
-        Text(text = "Hello $name")
+    fun MessageCard(msg: Message) {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(
+            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+        )
     }
 }
