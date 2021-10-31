0ccca04ae7d69746d85b05d72ade8fb401f4d687
yugo-miyabe <yuni42yuyuyu@gmail.com>
2021-10-31 22:12:10 +0900

    画像に枠線を追加

--- JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
+++ JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -32,24 +34,20 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MessageCard(msg: Message) {
-        // Add padding around our message
         Row(modifier = Modifier.padding(all = 8.dp)) {
             Image(
                 painter = painterResource(R.drawable.jetpack_compose_icon),
                 contentDescription = "Contact profile picture",
                 modifier = Modifier
-                    // Set image size to 40 dp
                     .size(40.dp)
-                    // Clip image to be shaped as a circle
                     .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
 
-            // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
 
             Column {
                 Text(text = msg.author)
-                // Add a vertical space between the author and message texts
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(text = msg.body)
             }
