cd63a43b8f1b00d3b5defbe09aab0cecc3644642
yugo-miyabe <yuni42yuyuyu@gmail.com>
2021-10-31 20:07:28 +0900

    テキストの値を設定する関数定義

--- JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
+++ JetpackComposeTutorial/app/src/main/java/com/fenrir/jetpackcomposetutorial/MainActivity.kt
@@ -3,18 +3,19 @@ package com.fenrir.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.fenrir.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello world!")
+            MessageCard("Android")
         }
+    }
+
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello $name")
     }
 }
