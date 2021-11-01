fc0da15b52fb1fc90d36a17cdde09988103e01cd
yugo-miyabe <yuni42yuyuyu@gmail.com>
2021-11-01 21:23:18 +0900

    新しいレイアウトを作成

--- LayoutsCodelab/app/src/main/java/com/fenrir/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/com/fenrir/layoutscodelab/MainActivity.kt
@@ -3,10 +3,15 @@ package com.fenrir.layoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.fenrir.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
@@ -34,5 +39,24 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     LayoutsCodelabTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun PhotographerCard() {
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+        // LocalContentAlpha is defining opacity level of its children
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutsCodelabTheme {
+        PhotographerCard()
     }
 }
