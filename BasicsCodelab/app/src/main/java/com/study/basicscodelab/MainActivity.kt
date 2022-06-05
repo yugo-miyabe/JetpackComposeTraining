61ed6f945a62dcf40bee528c997b242bd833b7b9
宮邉 勇悟 <miyabe@fenrir.co.jp>
2022-06-06 00:05:04 +0900

    タップ処理追加

--- BasicsCodelab/app/src/main/java/com/study/basicscodelab/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/study/basicscodelab/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,6 +35,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -44,8 +47,9 @@ private fun Greeting(name: String) {
             }
             OutlinedButton(
                 onClick = {
+                    expanded.value = !expanded.value
                 }) {
-                Text(text = "Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
